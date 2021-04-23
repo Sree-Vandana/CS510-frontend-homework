1724de3a05a37e8695b92ce10de566ed53edf917
Sree-Vandana <sreevandana.sv@gmail.com>
2021-04-23 14:25:03 -0700

    adding js solution for highlight text

--- hw3/03-count.js
+++ hw3/03-count.js
@@ -1,3 +1,21 @@
-// Enter your code here
+var original_data = document.getElementById('intro').innerHTML;
 
-console.log('Enter your code here');
+function highlight(ele) {
+
+  if(event.key === 'Enter') {
+    var highlight_val = ele.value;
+    console.log(highlight_val);
+
+    let data = original_data;
+    let highlight_regex = new RegExp(highlight_val, "ig");
+
+    let result = data.replace(highlight_regex, (match) => {
+      console.log(match);
+      return `<span style="background-color:yellow;">${match}</span>`;
+    });
+    document.getElementById("intro").innerHTML = result;
+
+    return false;
+  }
+}
+
